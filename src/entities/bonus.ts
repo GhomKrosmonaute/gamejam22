ae564d6e3bb5d64b333025bd973e829bc9f5c426
CamilleAbella <camille.abella@hotmail.fr>
2020-09-03 15:13:49 +0200

    continued bonuses implementation

--- src/entities/bonus.ts
+++ src/entities/bonus.ts
@@ -2,8 +2,7 @@ import * as entity from "booyah/src/entity";
 import Level from "../scenes/level";
 import Nucleotide from "./nucleotide";
 
-export class Bonus extends entity.EntityBase {
-  public selected = false;
+export abstract class Bonus extends entity.EntityBase {
   public name: string = "";
 }
 
@@ -11,7 +10,15 @@ export class Bonus extends entity.EntityBase {
 
 export class SwapBonus extends Bonus {
   name = "swap";
-  // todo: make animated this.grid.swap(n1, n2);
+
+  protected _setup() {
+    const level: Level = this._entityConfig.level
+
+    // todo: await mouse drag target
+      // todo: await mouse drop on target 2
+        // todo: level.grid.swap(n1, n2);
+        // todo: make animation
+  }
 }
 
 export class StarBonus extends Bonus {
@@ -22,26 +29,42 @@ export class StarBonus extends Bonus {
 
     let target: Nucleotide | null = null
 
+    const delay = 250
+
     new entity.EntitySequence([
       new entity.FunctionCallEntity(() => {
-        // await mouse click target
+        // todo: await mouse click target
+        target.state = "present"
       }),
+      new entity.WaitingEntity(delay),
       ...level.grid.getStarStages(target as Nucleotide).map((stage) => {
-        return new entity.FunctionCallEntity(() => {
-          for(const n of stage){
-            n.state = "present"
-          }
-        })
-      }),
+        return [
+          new entity.FunctionCallEntity(() => {
+            for(const n of stage){
+              n.state = "present"
+            }
+          }),
+          new entity.WaitingEntity(delay)
+        ]
+      }).flat(),
       new entity.FunctionCallEntity(() => {
         this._transition = entity.makeTransition()
       })
     ])
   }
-
-
 }
 
 export class KillBonus extends Bonus {
   name = "kill";
+
+  protected _setup() {
+    const level: Level = this._entityConfig.level
+
+    let target: Nucleotide | null = null
+
+    // todo: await mouse click target
+    target.state = "present"
+
+    this._transition = entity.makeTransition()
+  }
 }
