7be47befe190e6e7e1b179bf5d879dadc114792d
CamilleAbella <camille.abella@hotmail.fr>
2020-10-14 17:25:59 +0200

    move go button

--- src/entities/hud.ts
+++ src/entities/hud.ts
@@ -223,8 +223,8 @@ export class GoButton extends entity.CompositeEntity {
       ].texture
     );
     this.sprite.position.set(
-      this._entityConfig.app.view.width - 200,
-      this._entityConfig.app.view.height - 170
+      this._entityConfig.app.view.width * 0.785,
+      this._entityConfig.app.view.height * 0.887
     );
     this.sprite.interactive = true;
     this.sprite.buttonMode = true;
