e7e057526233a1132c4b39433f85e77725157c44
Jesse Himmelstein <jesse@heavenstone.net>
2021-05-17 15:44:42 +0200

    Added firebase analytics

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/analytics";
+import * as uuid from "uuid";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCHOlOOw9cOxF2eYmafVspVaG0t5RBnE9I",
@@ -13,13 +14,25 @@ const firebaseConfig = {
 
 let analytics: firebase.analytics.Analytics;
 let didInit = false;
+let userId: string;
 
 export function init(): void {
   if (didInit) return;
 
+  // Get UUID from storage
+  if (localStorage.getItem("userId")) {
+    userId = localStorage.getItem("userId");
+    console.log("Found userId", userId);
+  } else {
+    userId = uuid.v4();
+    localStorage.setItem("userId", userId);
+    console.log("Creating new userId", userId);
+  }
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   analytics = firebase.analytics();
+  analytics.setUserId(userId);
 
   didInit = true;
 }
