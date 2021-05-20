20d682d6e0ad77aefaae4dd029675c5c3d991910
Jesse Himmelstein <jesse@heavenstone.net>
2021-05-20 23:15:31 +0200

    commented out levels that aren't ready yet

--- src/levels.ts
+++ src/levels.ts
@@ -16,53 +16,55 @@ import * as crispr from "./crispr";
 declare var level: l.Level;
 
 export const levels = {
-  // Hard
-  "Big\nBoss": () =>
-    new l.Level("Big\nBoss", (ctx) => ({
-      gridShape: "mini",
-      forceMatching: false,
-      jokerCount: 1,
-      maxLife: 5,
-      portalsCount: 2,
-    })),
-  Caribbean: () =>
-    new l.Level("Caribbean", (ctx) => ({
-      gridShape: "fourIslands",
-      forceMatching: false,
-      clipCount: 4,
-      sequenceLength: 4,
-      portalsCount: 4,
-      jokerCount: 3,
-      maxLife: 10,
-      crispyBonusRate: 0.6,
-      initialBonuses: [
-        {
-          bonus: (ctx) => ctx.swapBonus,
-          quantity: 2,
-        },
-      ],
-      hooks: [
-        new l.Hook({
-          id: "intro",
-          event: "init",
-          once: true,
-          entity: new entity.FunctionCallEntity(() => {
-            ctx.activate(
-              new popup.TutorialPopup({
-                title: "Land in sight!",
-                content: `Loot the Caribbean treasures, collect at least ${ctx.options.score.max}!`,
-                image: "images/crispy.png",
-                imageHeight: 200,
-                popupOptions: {
-                  minimizeOnClose: false,
-                  coolDown: 2000,
-                },
-              })
-            );
-          }),
-        }),
-      ],
-    })),
+  // TODO: Joker doesn't work yet
+
+  // // Hard
+  // "Big\nBoss": () =>
+  //   new l.Level("Big\nBoss", (ctx) => ({
+  //     gridShape: "mini",
+  //     forceMatching: false,
+  //     jokerCount: 1,
+  //     maxLife: 5,
+  //     portalsCount: 2,
+  //   })),
+  // Caribbean: () =>
+  //   new l.Level("Caribbean", (ctx) => ({
+  //     gridShape: "fourIslands",
+  //     forceMatching: false,
+  //     clipCount: 4,
+  //     sequenceLength: 4,
+  //     portalsCount: 4,
+  //     jokerCount: 3,
+  //     maxLife: 10,
+  //     crispyBonusRate: 0.6,
+  //     initialBonuses: [
+  //       {
+  //         bonus: (ctx) => ctx.swapBonus,
+  //         quantity: 2,
+  //       },
+  //     ],
+  //     hooks: [
+  //       new l.Hook({
+  //         id: "intro",
+  //         event: "init",
+  //         once: true,
+  //         entity: new entity.FunctionCallEntity(() => {
+  //           ctx.activate(
+  //             new popup.TutorialPopup({
+  //               title: "Land in sight!",
+  //               content: `Loot the Caribbean treasures, collect at least ${ctx.options.score.max}!`,
+  //               image: "images/crispy.png",
+  //               imageHeight: 200,
+  //               popupOptions: {
+  //                 minimizeOnClose: false,
+  //                 coolDown: 2000,
+  //               },
+  //             })
+  //           );
+  //         }),
+  //       }),
+  //     ],
+  //   })),
   // Timed: () =>
   //   new l.Level("Timed", (ctx) => {
   //     ctx.playTime = 0;
@@ -106,53 +108,55 @@ export const levels = {
   //   }),
 
   // Medium
-  "Medium\nBoss": () => new l.Level("Medium\nBoss", {}),
-  "Chrono\nPortal": () =>
-    new l.Level("Chrono\nPortal", (context) => ({
-      variant: "fall",
-      gridShape: "medium",
-      forceMatching: true,
-      portalsCount: 2,
-      clipCount: 3,
-      gaugeRings: [
-        (context) =>
-          context.bonusesManager.add(
-            context.swapBonus,
-            1,
-            new PIXI.Point(200, -2000)
-          ),
-        (context, ring) =>
-          context.activate(
-            new entity.EntitySequence([
-              new entity.FunctionCallEntity(() => {
-                context.bonusesManager.add(
-                  context.timeBonus,
-                  1,
-                  new PIXI.Point(500, -2000)
-                );
-              }),
-            ])
-          ),
-      ],
-      hooks: [
-        new l.Hook({
-          id: "go title",
-          event: "injectedSequence",
-          entity: new entity.FunctionCallEntity(() => {
-            if (context.isEnded && !context.finished)
-              context.activate(anim.title(context.container, "Go!"));
-          }),
-        }),
-      ],
-    })),
-  "Four\nIslands": () =>
-    new l.Level("Four\nIslands", (context) => ({
-      gridShape: "fourIslands",
-      forceMatching: true,
-      clipCount: 3,
-      sequenceLength: 5,
-      portalsCount: 4,
-    })),
+  // TODO: Doesn't look like a boss
+  // "Medium\nBoss": () => new l.Level("Medium\nBoss", {}),
+
+  // "Chrono\nPortal": () =>
+  //   new l.Level("Chrono\nPortal", (context) => ({
+  //     variant: "fall",
+  //     gridShape: "medium",
+  //     forceMatching: true,
+  //     portalsCount: 2,
+  //     clipCount: 3,
+  //     gaugeRings: [
+  //       (context) =>
+  //         context.bonusesManager.add(
+  //           context.swapBonus,
+  //           1,
+  //           new PIXI.Point(200, -2000)
+  //         ),
+  //       (context, ring) =>
+  //         context.activate(
+  //           new entity.EntitySequence([
+  //             new entity.FunctionCallEntity(() => {
+  //               context.bonusesManager.add(
+  //                 context.timeBonus,
+  //                 1,
+  //                 new PIXI.Point(500, -2000)
+  //               );
+  //             }),
+  //           ])
+  //         ),
+  //     ],
+  //     hooks: [
+  //       new l.Hook({
+  //         id: "go title",
+  //         event: "injectedSequence",
+  //         entity: new entity.FunctionCallEntity(() => {
+  //           if (context.isEnded && !context.finished)
+  //             context.activate(anim.title(context.container, "Go!"));
+  //         }),
+  //       }),
+  //     ],
+  //   })),
+  // "Four\nIslands": () =>
+  //   new l.Level("Four\nIslands", (context) => ({
+  //     gridShape: "fourIslands",
+  //     forceMatching: true,
+  //     clipCount: 3,
+  //     sequenceLength: 5,
+  //     portalsCount: 4,
+  //   })),
 
   // Easy
   Boss: () =>
@@ -749,12 +753,12 @@ export type Levels = typeof levels;
 export const sections = {
   Intro: [levels.Tutorial],
   Easy: [levels.Classic, levels.Chrono, levels.Zen, levels.Boss],
-  Medium: [
-    levels["Four\nIslands"],
-    levels["Chrono\nPortal"],
-    levels["Medium\nBoss"],
-  ],
-  Hard: [levels.Caribbean, levels["Big\nBoss"]],
+  // Medium: [
+  //   levels["Four\nIslands"],
+  //   levels["Chrono\nPortal"],
+  // levels["Medium\nBoss"],
+  // ],
+  // Hard: [levels.Caribbean, levels["Big\nBoss"]],
 };
 
 export type SectionName = keyof typeof sections;
